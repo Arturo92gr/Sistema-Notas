e69228d7c06734f2e3bb4cf143a1bfa0d4ad010b
Arturo <149501608+Arturo92gr@users.noreply.github.com>
2024-11-07 13:03:07 +0100

    Actualización contenido de clase

---
+++ server/interfaces/model_1.js
@@ -0,0 +1,38 @@
+export const model1 = {
+    panels: [
+        {
+            panelName: 'panel1',
+            panelDescription: 'panel1 description',
+            panelWidth: '100',
+            panelHeight: '100',
+            cssData: 'panel_style_1',
+            components: [
+                {
+                    type: 'boton',
+                    father: 'panel1',
+                    content: 'Pulsa aquí',
+                    position: '0,0'
+                }
+            ]
+        }
+    ],
+    panels2: [
+        {
+            panelName: 'panel1',
+            panelDescription: 'panel1 description',
+            panelWidth: '100',
+            panelHeight: '100',
+            cssData: 'panel_style_1',
+            components: [
+                {
+                    type: 'boton',
+                    father: 'panel1',
+                    content: 'Pulsa aquí',
+                    position: '0,0'
+                }
+            ]
+        }
+    ]
+
+};
+
