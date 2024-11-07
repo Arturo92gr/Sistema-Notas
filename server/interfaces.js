8d57746004e0978cf669437f74e3e4675e287e77
Arturo <149501608+Arturo92gr@users.noreply.github.com>
2024-11-07 10:01:52 +0100

    Estructura de datos json

--- server/interfaces.js
+++ server/interfaces.js
@@ -1,11 +1,14 @@
-interfaz1: [
-    {
-         elemento1 : "Interfaz 1",
-     }
- ]
- 
- interfaz2: [
-     { 
-         elemento1 : "Interfaz 2",
-     }
-  ]
+const interfaces = {
+    interfaz1: [
+        {
+            elemento1: "Interfaz 1",
+        }
+    ],
+
+    interfaz2: [
+        {
+            elemento1: "Interfaz 2",
+        }
+    ]
+};
+export { interfaces };
